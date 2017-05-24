466cb5e30113964704d9246e8a2ccf0321d71f3b
Unknown <doivicle@gmail.com>
2017-05-24 15:08:55 -0700

    WaveProjectile added for any direction

--- DimensionalBeats/DimensionalBeats/Controllers/Projectile Controllers/ProjectileWave.cs
+++ DimensionalBeats/DimensionalBeats/Controllers/Projectile Controllers/ProjectileWave.cs
@@ -15,15 +15,18 @@ namespace DimensionalBeats.Controllers.Projectile_Controllers {
             this._lifespan = lifespan;
             this._theta = theta;
             this._velocity = velocity;
+            _deltaTheta = 0;
         }
 
         public void update() {
             CollisionResult res;
 
             float deltaX = _velocity * Game1.TILE_SIZE * Mathf.cos(_theta);
-            float deltaY = _velocity * Game1.TILE_SIZE * Mathf.sin(_deltaTheta);
+            float deltaY = _velocity * Game1.TILE_SIZE * Mathf.sin(_theta);
+            Vector2 lineVector = new Vector2(deltaX, deltaY);
+            Vector2 pulseVector = new Vector2(-deltaY, deltaX) * Mathf.sin(_deltaTheta);
 
-            _mover.move(new Vector2(deltaX, deltaY) * Time.deltaTime, out res);
+            _mover.move((lineVector + pulseVector) * Time.deltaTime, out res);
 
             _deltaTheta += Mathf.deg2Rad * 15;
             if (_deltaTheta >= MathHelper.TwoPi) _deltaTheta = 0;
